1aee46cfe3175a962e919b8cc9bb0e09ebf25eeb
Markturt <praisem52@gmail.com>
2024-10-07 14:14:19 +0100

    Added Mobile Design

---
+++ JS/main.js
@@ -0,0 +1,41 @@
+
+document.addEventListener("readystatechange",(e)=>{
+    if(document.readyState ==="complete"){
+        const menu =document.querySelector(".menu");
+
+        let requestData = async ()=>{
+            const request = await fetch('../data.json');
+            const response = await request.json();
+         
+            return response
+        }
+
+
+        let fillMenuContainer =(()=>{
+            requestData().then((items)=>{
+                    let fillMenu =items.map((item)=>{
+                        return  `<div class="menu-item" id="item_${item.category}">
+                                    <div class="menu-img">
+                                        <img src="${item.image.desktop}" alt="${item.name}" width="502" height="480">
+                                        <div class="add">
+                                            <img src="images/icon-add-to-cart.svg" alt="cart icon" class="cart">
+                                            <span>Add to Cart</span>
+                                        </div>
+                                    </div>
+                                    <h2 class="menu-heading">${item.category}</h2>
+                                    <h3 class="menu-subheading">${item.name}</h3>
+                                    <span class="menu-price">&dollar;${item.price.toFixed(2)}</span>
+                                </div>`;
+                        ;
+                    }).join("");
+                    
+                    menu.innerHTML = fillMenu;
+                })
+        })()
+    }
+})
+
+
+
+
+
